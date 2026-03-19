bc000ab499bafea603a1677af42893fae821fb7c
EeroLai <EeroLai@users.noreply.github.com>
2026-03-19 01:28:04 +0000

    deploy: 1eb8a81c9e4137d686347a6a563d1d41203aca7e

---
+++ _app/immutable/chunks/paths.a044050a.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1ab0yt4)==null?void 0:s.base)??"/timeless-jewels-zh-tw";var e;const a=((e=globalThis.__sveltekit_1ab0yt4)==null?void 0:e.assets)??t;export{a,t as b};
